e9c5db095402fa04cb1ca983f2a093c68dfe29c4
TijmenSchip <86233743+TijmenSchip@users.noreply.github.com>
2021-06-24 13:26:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.animation as animation
+
+class Automaat:
+    '''Een cellulaire automaat met rooster n
+    een cel heeft m toestanden
+    alle cellen in het rooster hebben dezelfde regels '''  
+
+    '''Te doen: cellen maken, regels maken'''
+
+    def __init__(self, dimensies, omvang, toestanden, randvoorwaarden):
+        self.rooster = Automaat.rooster(self,dimensies,omvang)
+        self.toestanden = Automaat.toestanden(self,toestanden)
+        self.randvoorwaarden = Automaat.randvoorwaarden(self,randvoorwaarden)
+
+    def rooster(self, dim, n): 
+        '''maakt een rooster van lengte n in een gespecificeerde dimensie'''
+        vorm = tuple(n for i in range(dim))
+        return np.zeros(vorm)
+
+    def toestanden(self, m): #toestand van een cel, m mogelijkheden
+        return [i for i in range(m)]
+
+    def randvoorwaarden(self,randvoorwaarde):
+        if randvoorwaarde == "periodiek":
+            '''rechterbuur van laatste cel is eerste cel en linkerbuurt eerste is laatste'''
+            return 0
+        return 0
